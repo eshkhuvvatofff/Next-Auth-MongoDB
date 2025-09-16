fa08227e3e97dfb2b1b346aa635398ca115021e5
Asliddin <Eshkhuvvatofff@gmail.com>
2025-09-16 18:42:10 +0500

    metadatas add

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,20 @@
 import LayoutWrapper from "@/components/layout/LayoutWrapper";
 import "./globals.css";
 
+export const metadata = {
+  title: "Next-Auth-Mongodb",
+  description: "Next-auth-mongodb learning",
+  icons: {
+    icon: "/Logo.png",
+  },
+};
+
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning
+
+    >
       <body>
         <LayoutWrapper>
           {children}
